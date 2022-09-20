08522f822d6b096ab433ea26f25457efd860f600
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2022-09-20 17:07:27 -0300

    Adding a new exercise

---
+++ UsingLibraries/ex0015.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia um número real qualquer pelo teclado
+# e mostre na tela a sua porção inteira
+
+# OUTPUT:
+# Digite um número: 6.127
+# O número 6.127 tem a parte inteira 6.
+
