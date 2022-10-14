708c298668a232b2bf75c4bb5a6be95aaecfdeeb
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2022-10-14 00:08:21 -0300

    Resolving exercises using while loops

--- 8.RepeticoesWhile/ex0062.py
+++ 8.RepeticoesWhile/ex0062.py
@@ -4,3 +4,14 @@ e mostre na tela os n primeiros elementos de uma Sequência
 de Fibonacci
 """
 escolha = int(input('Informe quantos termos a sequência terá: '))
+
+somaFibonacci = 0
+termoAnterior = 1
+termoPosterior = 0
+
+for i in range(0,escolha):
+        somaFibonacci = termoAnterior + termoPosterior
+        termoAnterior = termoPosterior
+        termoPosterior = somaFibonacci
+        print(somaFibonacci,end=' ')
+print('FIM')
---
+++ 8.RepeticoesWhile/ex0063.py
@@ -0,0 +1,6 @@
+"""
+Crie um programa que leia vários números inteiros. O programa só vai parar
+quando o usuário digitir o valor 999, quie é a condição de parada. No final,
+mostre quantos números foram digitados e qual foi a soma entre elas.
+(desconsiderando o flag)
+"""
