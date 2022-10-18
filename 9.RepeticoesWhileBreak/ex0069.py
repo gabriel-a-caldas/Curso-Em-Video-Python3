649f26b3c30e0bf754b8b644a42cb942e845658a
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2022-10-18 00:00:52 -0300

    Resolving exercises using break

--- 9.RepeticoesWhileBreak/ex0069.py
+++ 9.RepeticoesWhileBreak/ex0069.py
@@ -9,27 +9,27 @@ c) Qual é o nome do produto mais barato.
 
 """
 
-somaTotalProdutos = contadorProdutoMilReais = produtoMaisBarato = 0
-nomeProdutoMaisBarato = ''
+somaTotalProdutos = contadorMil = contador = menorValor = 0
+produtoMaisBarato = ' '
 
+print('='*22)
+print('SUPERMERCADO MERCADÃO')
+print('='*22)
 while True:
-    print('='*22)
-    print('SUPERMERCADO MERCADÃO')
-    print('='*22)
-    nomeProduto = str(input('Nome do produto: ')).strip()
+    produto = str(input('Produto: ')).strip().upper()
     precoProduto = float(input('Preço: R$'))
-    produtoMaisBarato = precoProduto
-    escolha = str(input('DESEJA CONTINUAR? [S/N]: ')).strip().upper()
-    while escolha != 'S' and escolha != 'N':
-        escolha = str(input('DESEJA CONTINUAR? [S/N]: ')).strip().upper()
     somaTotalProdutos += precoProduto
+    contador += 1
+    if precoProduto > 1000:
+        contadorMil += 1
+    if contador == 1 or precoProduto < menorValor:
+        menorValor = precoProduto
+        produtoMaisBarato = produto
+    escolha = ' '
+    while escolha not in 'SN':
+        escolha = str(input('DESEJA CONTINUAR? [S/N]: ')).strip().upper()[0]
     if escolha == 'N':
         break
-    if precoProduto > 1000:
-        contadorProdutoMilReais += 1
-    if precoProduto < produtoMaisBarato:
-        produtoMaisBarato = precoProduto
-        nomeProdutoMaisBarato = nomeProduto
 print(f'O total da compra foi: R${round(somaTotalProdutos,2)}.')
-print(f'Temos {contadorProdutoMilReais} produtos custando mais de R$1000,00.')
-print(f'O produto mais barato: {nomeProdutoMaisBarato} que custa R${produtoMaisBarato}')
+print(f'Temos {contadorMil} produtos custando mais de R$1000,00.')
+print(f'O produto mais foi: {produtoMaisBarato} barato custa R${menorValor}')
