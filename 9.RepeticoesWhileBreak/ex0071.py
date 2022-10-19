d699aa9b0a6bf75384534ddbd4158feed2c9a8d6
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2022-10-18 22:29:24 -0300

    Resolving exercises using break

--- 9.RepeticoesWhileBreak/ex0071.py
+++ 9.RepeticoesWhileBreak/ex0071.py
@@ -6,23 +6,25 @@ e o programa vai informar quantas cédulas de cada valor serão entregues.
 OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
 """
 
-quantidadeNotasUm = quantidadeNotasDez = quantidadeNotasVinte = quantidadeNotasCinquenta = 0
-unidade = centena = dezena = milhar = total = 0
+quantidade = int(input('VALOR DE SAQUE: '))
+total = quantidade
+cedula = 50
+totalCedulas = 0
 
 while True:
-    quantidade = int(input('VALOR DE SAQUE: '))
-
-    unidade = quantidade // 1 % 10
-    dezena = (quantidade // 10 % 10)
-    centena = (quantidade // 100 % 10)
-    milhar = (quantidade // 1000 % 100)
-
-    if dezena%2 == 0:
-        quantidadeNotasVinte = (dezena * 10)/ 20
-        print(f'Foram usadas: {quantidadeNotasVinte} notas de R$20.')
-        break
+    if total >= cedula:
+        total -= cedula
+        totalCedulas += 1
     else:
-        quantidadeNotasVinte = (dezena * 10) // 20
-        quantidadeNotasDez = ((dezena * 10) // 10) - quantidadeNotasVinte*2
-        print(f'Foram usadas: {quantidadeNotasVinte} notas de R$20 e {quantidadeNotasDez} notas de R$10.')
-    break
+        if totalCedulas > 0:
+            print(f'Total de {totalCedulas} de R${cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totalCedulas = 0
+        if total == 0:
+            break
+print('CONFIRA O DINHEIRO SACADO.')
