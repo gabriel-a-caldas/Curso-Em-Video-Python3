275df96791f9e987baf65e5c9f08f5a0c22171e1
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2022-10-21 16:56:36 -0300

    Resolving exercises using tuples

--- 10.Tuplas/ex0077.py
+++ 10.Tuplas/ex0077.py
@@ -2,3 +2,9 @@
 Crie um programa que tenha uma tupla com várias palavaras.
 Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 """
+parametroParadaTupla = int(input('Informe quantas palavras deseja inserir: '))
+
+tuplaPalavrasAleatorias = tuple(str(input('Insira uma palavra: '))for c in range(0,parametroParadaTupla))
+
+tuplaVogais = ('a','e','i','o','u')
+
