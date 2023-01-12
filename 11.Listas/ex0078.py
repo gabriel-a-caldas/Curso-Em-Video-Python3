f0917afe98bd9cf1b42ef08617d432e967b9564d
gabriel-a-caldas <gabrieldacaldas@gmail.com>
2023-01-12 10:50:30 -0300

    dev/correcting the sorting list

--- 11.Listas/ex0078.py
+++ 11.Listas/ex0078.py
@@ -12,21 +12,23 @@ listaAleatoria = list()
 while True:
     elementoDaLista = int(input('Digite um número: '))
 
+
     if elementoDaLista not in listaAleatoria:
         listaAleatoria.append(elementoDaLista)
         print('Computando valor...')
         
-        sleep(2)
+        sleep(1)
         system('cls')
 
         print('Valor computador.')
     else:
-        print('Numero duplicado! Não vou adicionar...')
+        print('Valor duplicado! Não vou adicionar...')
         
-        sleep(3)
+        sleep(1)
         system('cls')
-
+        
     resposta = str(input('Deseja continuar? [S/N] '))
     if resposta in 'Nn':
         break
-print(listaAleatoria)
+listaAleatoria.sort() # Organia a lista em ordem crescente.
+print(f'A lista digitada foi: {listaAleatoria}.')
