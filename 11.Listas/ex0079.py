51ba1ddfcc38c6ed212a1e770bf5056de8b1733b
gabriel-a-caldas <gabrieldacaldas@gmail.com>
2023-01-12 17:08:42 -0300

    dev/adding a new exercise

---
+++ 11.Listas/ex0079.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa em que o usuário possa digitar cinco valores numéricos e
+cadastr-os em uma lista, já na posição correta de inserção (sem usar o sort()).
+
+No final, mostre a lista ordenda na tela.
+"""
+
+listaOrdenada = list()
+
