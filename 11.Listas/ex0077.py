f6ab12edea4c7bbfc0bbec4dc47a9d5db7fe16a6
gabriel-a-caldas <gabrieldacaldas@gmail.com>
2023-01-12 15:46:22 -0300

    dev/correcting the logic of the exercise

--- 11.Listas/ex0077.py
+++ 11.Listas/ex0077.py
@@ -3,18 +3,38 @@ Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas
 posições na lista.
 """
-lista = list(int(input('Insira um número: '))for c in range(0,5))
+
+from os import system
+
+listaAleatoria = list()
 
 maior = menor = 0
-print('A lista: ',end='')
-for pos, item in enumerate(lista):
+
+for numero in range(0,5):
+    numeroDaLista = int(input(f'Insira o {numero+1}º valor: '))
+    listaAleatoria.append(numeroDaLista)
+
+for pos, item  in enumerate(listaAleatoria):
     if pos == 0:
-        maior = item
-        menor = item
+        maior = menor = item
     else:
         if maior < item:
             maior = item
         if menor > item:
             menor = item
-    print(f'{pos}',end=' ')
-print(f'\nO maior valor digitado foi {maior} e o menor {menor}.')
+
+system('cls')
+
+print(f'Na lista: {listaAleatoria}...')
+
+print(f'O maior valor digitado foi: {maior}, nas posições: ',end=' ')
+
+for posicao, valor in enumerate(listaAleatoria):
+    if valor == maior:
+        print(f'{posicao}...',end=' ')
+
+print(f'\nO menor valor digitado foi: {menor}, nas posições: ',end=' ')
+
+for posicao, valor in enumerate(listaAleatoria):
+    if valor == menor:
+        print(f'{posicao}...',end=' ')
