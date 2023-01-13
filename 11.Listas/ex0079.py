fc8249430c2a326fc67c08af7b10f8a8a29ca2a5
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2023-01-13 00:07:22 -0300

    dev/working on the logic of the exercise

--- 11.Listas/ex0079.py
+++ 11.Listas/ex0079.py
@@ -7,3 +7,13 @@ No final, mostre a lista ordenda na tela.
 
 listaOrdenada = list()
 
+while True:
+    listaOrdenada.append(int(input('Digite um valor: ')))
+    for posicao, elementoDaLista in enumerate(listaOrdenada):
+        if posicao == 0:
+            print('Adicionado ao final da lista...')
+        else:
+            if listaOrdenada[posicao] > listaOrdenada[posicao+1]: #Verifica se o valor é maior.
+                listaOrdenada[posicao]
+    if len(listaOrdenada) == 5:
+        break
