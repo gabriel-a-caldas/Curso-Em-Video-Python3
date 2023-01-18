2b7ecb169d8abb35a03b544f57e7400d3afa11d4
gabriel-a-caldas <gabrieldacaldas@gmail.com>
2023-01-18 14:31:19 -0300

    dev/resolving the exercise about lists

--- 11.Listas/ex0079.py
+++ 11.Listas/ex0079.py
@@ -4,6 +4,26 @@ cadastr-os em uma lista, já na posição correta de inserção (sem usar o sort
 
 No final, mostre a lista ordenda na tela.
 """
+from time import sleep
+
+aux = 0
 
 listaOrdenada = list()
 
+for contador in range(0,5):
+
+    numeroLista = int(input('Insira um número: '))
+
+    if contador == 0 or numeroLista > listaOrdenada[-1]:
+        listaOrdenada.append(numeroLista)
+        print('Adicionado ao final da lista...')
+    else:
+        posicao = 0
+        while posicao < len(listaOrdenada):
+            if numeroLista <= listaOrdenada[posicao]:
+                listaOrdenada.insert(posicao, numeroLista)
+                print(f'Adicionado na posição: {posicao}')
+                break
+            posicao += 1
+print(f'Os valores adicionados na lista foram: {listaOrdenada}')
+        
