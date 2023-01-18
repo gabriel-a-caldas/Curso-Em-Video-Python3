7a20097ec5a625dc40e2cfcefcf9c1527c9dd5a6
gabriel-a-caldas <gabrieldacaldas@gmail.com>
2023-01-18 16:15:25 -0300

    dev/resolving commit conflicts

--- 4.Strings/ex024.py
+++ 4.Strings/ex024.py
@@ -3,11 +3,4 @@
 
 nomeCompleto = str(input('Informe o nome completo: ')).upper().strip()
 
-<<<<<<< HEAD
-if 'SILVA' in nomeCompleto:
-    print('O seu nome possui Silva!')
-else:
-    print('O seu nome não possui Silva!')
-=======
 print('Seu nome tem Silva? {}'.format('SILVA' in nomeCompleto))
->>>>>>> bd18eac93bc54cb541127c7aa5e6f2b359103702
--- 4.Strings/ex025.py
+++ 4.Strings/ex025.py
@@ -3,18 +3,6 @@
 # Em que posição ela aparece a primeira vez.
 # Em que posição ela aparece a última vez.
 
-<<<<<<< HEAD
-from re import A
-
-
-frase = str(input('Insira a frase: ')).upper()
-
-contaLetrasA = frase.count('A')
-primeiraPosicao = frase.find('A')
-
-print(f'Número de letras a: {contaLetrasA}')
-print(f'Posição número: {primeiraPosicao}')
-=======
 frase = str(input('Insira a frase: ')).upper().strip()
 
 contaLetrasA = frase.count('A')
@@ -25,4 +13,3 @@ ultimaPosicaoLetraA = frase.rfind('A')
 print(f'A letra A aparece: {contaLetrasA} vezes.')
 print(f'A primeira letra A aparece na posição: {primeiraPosicaoLetraA+1}')
 print(f'A última letra A aparece na posição:  {ultimaPosicaoLetraA+1}')
->>>>>>> bd18eac93bc54cb541127c7aa5e6f2b359103702
