4d0349dba424ca6f259bfdeb8fb12f4956706404
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2023-01-26 22:13:55 -0300

    Uploading the exercise

--- 11.Listas/ex0084.py
+++ 11.Listas/ex0084.py
@@ -1,3 +1,5 @@
 """
-
+Crie um programa onde o usuárioo possa digitar sete valores numéricos
+e cadsatre-os em uma lista única que mantenha separados os valores pares
+e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 """
