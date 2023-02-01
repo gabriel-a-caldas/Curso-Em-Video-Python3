0055edfbf942bf928fcfd76e3431b110d39ee24b
gabriel-a-caldas <gabrieldeacaldas@gmail.com>
2023-02-01 10:09:38 -0300

    Resolving exercises using lists

--- 11.Listas/ex0084.py
+++ 11.Listas/ex0084.py
@@ -3,3 +3,22 @@ Crie um programa onde o usuárioo possa digitar sete valores numéricos
 e cadsatre-os em uma lista única que mantenha separados os valores pares
 e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 """
+totalNumeros = [[],[]]
+
+for indice in range(1,8):
+
+    numeroUsuario = int(input(f'Digite o {indice}º valor: '))
+
+    if numeroUsuario % 2 == 0:
+        totalNumeros[0].append(numeroUsuario)
+    else:
+        totalNumeros[1].append(numeroUsuario)
+
+totalNumeros[0].sort()
+totalNumeros[1].sort()
+
+print(f'Os valores pares digitados foram: {totalNumeros[0]}')
+print(f'Os valores ímpares digitados foram: {totalNumeros[1]}')
+
+
+    
--- 11.Listas/ex0085.py
+++ 11.Listas/ex0085.py
@@ -1,3 +1,39 @@
 """
-
+Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores
+lidos pelo teclado.
+    [][][]
+    [][][]
+    [][][]
+No final, mostre a matriz na tela, com a formatação correta.
 """
+
+from os import system
+
+matriz = [[], [], []]
+cadastroValores = []
+
+for linhas in range(1,4):
+
+    for colunas in range(1,4):
+
+        valor = int(input(f'Digite o valor da {linhas}ª linha x {colunas}ª coluna da matriz: '))
+        
+        if linhas == 1:
+            cadastroValores.append(valor)
+            matriz[0].append(cadastroValores[:])
+        elif linhas == 2:
+            cadastroValores.append(valor)
+            matriz[1].append(cadastroValores[:])
+        elif linhas == 3:
+            cadastroValores.append(valor)
+            matriz[2].append(cadastroValores[:])
+        cadastroValores.clear()
+
+system('cls')
+
+print('Sua matriz: ')
+
+for linhas in range(0,3):
+    for colunas in range(0,3):
+        print(f'{matriz[linhas][colunas]}',end='')
+    print()
