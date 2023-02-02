f0dd04555f4fed2157065777871b4b998229d40f
gabriel-a-caldas <gabrieldeacaldas@gmail.com>
2023-02-02 16:20:14 -0300

    dev/Adding the exercise base

--- 11.Listas/ex0088.py
+++ 11.Listas/ex0088.py
@@ -3,3 +3,36 @@ Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em u
 lista compsosta. No final, mostre um boletim contendo a média de cada um e permita
 que o usuário possa mostrar as notas de cada aluno individualmente.
 """
+
+from os import system
+
+cadastroAlunos = []
+cadastroNotas = []
+totalAlunos = []
+media = 0
+
+while True:
+
+    nome = str(input('Nome: ')).strip()
+    notaUm = int(input('Nota 1: '))
+    notaDois = int(input('Nota 2: '))
+
+    cadastroAlunos.append(nome)
+    cadastroNotas.append(notaUm)
+    cadastroNotas.append(notaDois)
+    cadastroAlunos.append(cadastroNotas[:])
+    
+    totalAlunos.append(cadastroAlunos[:])
+
+    cadastroAlunos.clear()
+    cadastroNotas.clear()
+
+    resposta = str(input('Quer continuar? [S/N] ')).strip()
+
+    if resposta in 'Nn':
+        break
+
+system('cls')
+
+
+
