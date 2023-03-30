3bedd7ea734e4156f9efe38258e01b51ac9c1f28
gabriel-a-caldas <gabrieldeacaldas@gmail.com>
2023-03-30 11:37:52 -0300

    dev/resolving exercise using modules

---
+++ 14. Modulos/ex0111/principal.py
@@ -0,0 +1,14 @@
+"""
+Adicione ao módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre
+no tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+"""
+from utilidadescev import moeda
+from os import system
+
+system('cls')
+
+preco = float(input('Digite o preço: R$'))
+valorParaAumentar = int(input(f'Informe quanto se deve aumentar de {moeda.floatToCurrency(preco)}: '))
+valorParaDiminuir = int(input(f'Informe quanto se deve diminuir de {moeda.floatToCurrency(preco)}: '))
+
+moeda.resumo(preco,valorParaAumentar,valorParaDiminuir)
